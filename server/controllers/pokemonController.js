a6369d1cb1cb39d875ee59f1d866ed7fdd9919ed
Jiaxin <jl2458@rutgers.edu>
2022-01-07 21:28:01 -0500

    add pokemon router & error handling

--- server/controllers/pokemonController.js
+++ server/controllers/pokemonController.js
@@ -1,7 +1,28 @@
+const fetch = require('node-fetch');
 const pokemon = {};
-pokemon.getPokemon = (req, res, next) => {
+pokemon.getPokemon = async (req, res, next) => {
     const { pokemon_name } = req.params;
-    res.locals.data = pokemon_name;
-    return next();
+    const base_URI = `https://pokeapi.co/api/v2/pokemon/${pokemon_name}`;
+    try {
+        const response = await fetch(base_URI);
+        let data = await response.json();
+        data = {
+            id: data.id,
+            type: data.types.map((a) => a.type.name),
+            pic: data.sprites.front_default,
+            hp: data.stats[0].base_stat,
+            attack: data.stats[1].base_stat,
+            defense: data.stats[2].base_stat,
+        };
+        res.locals.data = data;
+        return next();
+    } catch (err) {
+        next({
+            log: `pokemonController.getPokemon: ERROR: ${err}`,
+            message: {
+                err: 'Error: Bad fetch pokemon request. Check server logs fo details.',
+            },
+        });
+    }
 };
 module.exports = pokemon;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ app.use(morgan('tiny'));
 app.use(express.json());
 // router for Pokemon
 app.use('/pokemon', pokemonRoute);
+// default error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error (default error handler)',
+        status: 400,
+        message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
 // start server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
