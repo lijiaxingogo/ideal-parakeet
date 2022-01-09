2b4e3bb230f877bc5363e2a75ee51dbefcc7f331
Jiaxin <jl2458@rutgers.edu>
2022-01-09 16:46:06 -0500

    update response-add pokemon name

--- frontend/src/components/Display/Display.js
+++ frontend/src/components/Display/Display.js
@@ -1,4 +1,9 @@
+import React, { useState, useContext } from 'react';
+import { PokemonContext } from '../../context/pokemonContext';
+
 const Display = () => {
+    const { pokemonList } = useContext(PokemonContext);
+    console.log(pokemonList);
     return <div>this is display</div>;
 };
 export default Display;
