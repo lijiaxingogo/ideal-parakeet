dda87c5ef1c56990c118476d73e21c8e6de69553
jessie <49336545+lijiaxingogo@users.noreply.github.com>
2025-06-22 15:21:08 -0400

    Update server/test_two_sum.py

--- server/test_two_sum.py
+++ server/test_two_sum.py
@@ -2,23 +2,20 @@ import unittest
 from two_sum import two_sum
 
 class TestTwoSum(unittest.TestCase):
-    def test_two_sum_basic(self):
+    def test_basic(self):
         nums = [2, 7, 11, 15]
         target = 9
-        expected = [0, 1]
-        self.assertEqual(two_sum(nums, target), expected)
+        self.assertEqual(two_sum(nums, target), [0, 1])
 
-    def test_two_sum_duplicate(self):
-        nums = [3, 2, 4, 3]
+    def test_no_solution(self):
+        nums = [3, 2, 4]
         target = 6
-        expected = [1, 3]
-        self.assertEqual(two_sum(nums, target), expected)
+        self.assertEqual(two_sum(nums, target), [])
 
-    def test_two_sum_no_solution(self):
-        nums = [1, 2, 3, 4]
-        target = 10
-        expected = []
-        self.assertEqual(two_sum(nums, target), expected)
+    def test_duplicate_elements(self):
+        nums = [3, 3]
+        target = 6
+        self.assertEqual(two_sum(nums, target), [0, 1])
 
 if __name__ == '__main__':
     unittest.main()
