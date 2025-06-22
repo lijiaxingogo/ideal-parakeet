e79660ca95d91dc522d0916da7f52f2d0b8370f6
jessie <49336545+lijiaxingogo@users.noreply.github.com>
2025-06-22 15:31:22 -0400

    Add/update server/test_two_sum.py

---
+++ server/test_two_sum.py
@@ -0,0 +1,21 @@
+import unittest
+from two_sum import two_sum
+
+class TestTwoSum(unittest.TestCase):
+    def test_basic(self):
+        nums = [2, 7, 11, 15]
+        target = 9
+        self.assertEqual(two_sum(nums, target), [0, 1])
+
+    def test_no_solution(self):
+        nums = [1, 2, 3, 4]
+        target = 10
+        self.assertEqual(two_sum(nums, target), [])
+
+    def test_duplicate_values(self):
+        nums = [3, 2, 4, 3]
+        target = 6
+        self.assertEqual(two_sum(nums, target), [0, 3])
+
+if __name__ == '__main__':
+    unittest.main()
